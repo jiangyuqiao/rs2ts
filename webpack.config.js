0004499cce7be78bcfe26d6f083eead114685e1d
jiangyuqiao <jiangyuqiao@foxmail.com>
2022-02-06 08:37:47 +0800

    monaco plugin options

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,7 @@ module.exports = {
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
     new MonacoWebpackPlugin({
+      features: ['!accessibilityHelp', '!colorPicker', '!snippet', '!codeAction', '!codelens', '!toggleHighContrast', '!toggleTabFocusMode'],
       languages: ['typescript', 'rust']
     })
   ],
